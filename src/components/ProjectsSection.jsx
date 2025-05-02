d31fce292a01966b59805b78e61cc2f1a9f361a6
Khai Pong Teoh <khaipongteoh@gmail.com>
2025-05-02 23:29:38 +0800

    add projects description and images

--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -3,30 +3,31 @@ import { ArrowRight, ExternalLink, Github } from "lucide-react";
 const projects = [
   {
     id: 1,
-    title: "SaaS Landing Page",
-    description: "A beautiful landing page app using React and Tailwind.",
-    image: "/projects/project1.png",
-    tags: ["React", "TailwindCSS", "Supabase"],
+    title: "Full Stack E-commerce Platform",
+    description:
+      "A platform to allow users to buy or view items via retail sales through online marketplaces and websites",
+    image: "/projects/coming-soon.jpg",
+    tags: ["Nextjs", "React", "TailwindCSS", "Payload", "Stripe"],
     demoUrl: "#",
     githubUrl: "#",
   },
   {
     id: 2,
-    title: "Orbit Analytics Dashboard",
+    title: "Admin Analytics Dashboard",
     description:
       "Interactive analytics dashboard with data visualization and filtering capabilities.",
-    image: "/projects/project2.png",
-    tags: ["TypeScript", "D3.js", "Next.js"],
+    image: "/projects/admin-dashboard.png",
+    tags: ["TypeScript", "Next.js", "Shadcn UI"],
     demoUrl: "#",
-    githubUrl: "#",
+    githubUrl: "https://github.com/KPTeoh/admin-dashboard",
   },
   {
     id: 3,
-    title: "E-commerce Platform",
+    title: "Pawtograph",
     description:
-      "Full-featured e-commerce platform with user authentication and payment processing.",
+      "A website dedicated to sharing and celebrating dog pictures, creating a community of dog enthusiasts.",
     image: "/projects/project3.png",
-    tags: ["React", "Node.js", "Stripe"],
+    tags: ["Nextjs", "Auth.js", "PostgreSQL", "Prisma"],
     demoUrl: "#",
     githubUrl: "#",
   },
@@ -100,7 +101,7 @@ export const ProjectsSection = () => {
           <a
             className="cosmic-button w-fit flex items-center mx-auto gap-2"
             target="_blank"
-            href="https://github.com/machadop1407"
+            href="https://github.com/kpteoh"
           >
             Check My Github <ArrowRight size={16} />
           </a>
